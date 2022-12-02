df23eac5c97bd033df28e36f128ba82f469ccef6
webFardin <webFardin@gmail.com>
2022-12-02 17:43:47 +0330

    some gaps changing in mobile version

--- script.js
+++ script.js
@@ -75,7 +75,6 @@ let worksSectionSubjectsData = [
     icon: `<svg xmlns="http://www.w3.org/2000/svg" style="width: 0.8rem" fill="currentColor" viewBox="0 0 16 16">
   <path d="M0 1a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm9 0a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V1zm0 9a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-5z"></path>
 </svg>`,
-    description: "Small components of websites",
     boxes: [
       {
         gif: "https://i.postimg.cc/nh5y2c5J/20221116-174755.gif",
@@ -101,7 +100,6 @@ let worksSectionSubjectsData = [
     icon: `<svg xmlns="http://www.w3.org/2000/svg" style="width: 0.8rem" fill="currentColor" viewBox="0 0 16 16">
     <path d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3zm1 5.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm8 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm1.5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM12 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM4 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
   </svg>`,
-    description: "Funny mini games",
     boxes: [
       {
         gif: "https://i.postimg.cc/httds8Vz/the-Last-Strawberry.gif",
@@ -116,13 +114,10 @@ let worksSectionSubjectsData = [
 worksSectionSubjectsData.forEach((item) => {
   worksSectionSubjects.innerHTML += `
   <div class="works-section-subject">
-            <div class="works-section-titles">
-              <h4>
+              <h4 class="works-section-titles">
                 ${item.icon}
                 ${item.name}
               </h4>
-              <h5>${item.description}</h5>
-            </div>
             <div class="works-section-boxes">
             
             </div>
